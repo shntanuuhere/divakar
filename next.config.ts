afbd4bf378749af996f45b1ef7ad8ccf395b37e1
Shantanu Sen Gupta <taleshereco@gmail.com>
2026-09-12 19:42:40 +0530

    added cdn for js and css

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: "https://static.hereco.in/assets/divakar",
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'media.hereco.in',
+        protocol: "https",
+        hostname: "media.hereco.in",
       },
       {
-        protocol: 'https',
-        hostname: 'www.hereco.in',
+        protocol: "https",
+        hostname: "www.hereco.in",
       },
     ],
   },
+
   async rewrites() {
     return [
       {
-        source: '/api/proxy/festival',
-        destination: 'https://www.hereco.in/api/festival',
+        source: "/api/proxy/festival",
+        destination: "https://www.hereco.in/api/festival",
       },
       {
-        source: '/api/proxy/lottie/:path*',
-        destination: 'https://media.hereco.in/festival-badges/:path*',
+        source: "/api/proxy/lottie/:path*",
+        destination: "https://media.hereco.in/festival-badges/:path*",
       },
     ];
   },
